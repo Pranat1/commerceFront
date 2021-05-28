5ebecc57672289ebe8d428cd62e40f964d4f2a6a
Pranat1 <pranat@lucknowmarbles.com>
2021-05-28 15:02:59 +0530

    latest

---
+++ src/core/Card.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import ShowImage from './ShowImage';
+import moment from 'moment';
+
+const Card = ({
+  product,
+  showViewProductButton = true,
+  showRemoveProductButton = false,
+  setRun = f => f,
+  run = undefined
+}) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
+  const showViewButton = showViewProductButton => {
+    return (
+      showViewProductButton && (
+        <Link to={`/product/${product._id}`} className="mr-2">
+          <button className="btn btn-outline-primary mt-2 mb-2 card-btn-1">View Product</button>
+        </Link>
+      )
+    );
+  };
+
+
+  const showStock = quantity => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock </span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of Stock </span>
+    );
+  };
+
+
+  return (
+    <div className="card ">
+      <div className="card-header card-header-1 ">{product.name}</div>
+      <div className="card-body">
+        <ShowImage item={product} url="product" />
+        <p className="card-p  mt-2">{product.description.substring(0, 100)} </p>
+        <p className="card-p black-10">Rs {product.price}</p>
+        <p className="black-9">Category: {product.category && product.category.name}</p>
+        <p className="black-8">Added on {moment(product.createdAt).fromNow()}</p>
+        {showStock(product.quantity)}
+        <br />
+
+        {showViewButton(showViewProductButton)}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "Rs0 to Rs60",
+        array: [0, 60]
+    },
+    {
+        _id: 2,
+        name: "Rs60 to Rs100",
+        array: [60, 100]
+    },
+    {
+        _id: 3,
+        name: "Rs100 to Rs150",
+        array: [100, 150]
+    },
+    {
+        _id: 4,
+        name: "Rs150 to Rs200",
+        array: [150, 200]
+    },
+    {
+        _id: 5,
+        name: "More than Rs200",
+        array: [200, 9999]
+    }
+];
